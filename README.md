3225f68ed0dae9db79f3bf4a222ae36c9b734688
Aseem Regmi <aseem.regmi22@gmail.com>
2018-06-16 08:47:08 +0545

    App

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # HelloWorld
 
-This is my first react application. It's fun to learn React. ;)
+This is my first react application. It's fun to learn React. ;) <br>
+To run this app first clone the repository. Install required node modules by npm using npm install. Then run npm start and app will start in local server at your default browser.
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">Hello World</header>
+        <p className="App-intro">
+          This is my first ever react app. It's fun to learn react.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
